2026846bd41b820b04a87b1624bdbc1811f22adc
olleholmgren <olle.holmgren@gmail.com>
2023-03-01 08:56:13 +0000

    add directions to rooms dictionary

--- rooms.py
+++ rooms.py
@@ -1,21 +1,41 @@
 
 number = '0'
 
+"""
+
+Controls for the game is n for north, s for south, e for east and finally - w for west.
+Numbers in dictionary is corrensponding to the room which you are navigating to.
+
+"""
+
 rooms = [
     {
         'number': 1,
         'name': 'The Tent',
-        'environment': 'green tent sheets(walls), a red sleeping bag, a hat with a feather'
+        'environment': 'green tent sheets(walls), a red sleeping bag, a hat with a feather',
+        'north': 
+        'south':
+        'east':
+        'west':
     },
     {
         'number': 2,
         'name': 'The Tent Entrance',
-        'environment': 'zip-lock'
+        'environment': 'zip-lock',
+        'north': 
+        'south':
+        'east':
+        'west':
+
     },
     {
         'number': 3,
         'name': 'The river',
-        'environment': 'Water rushing at splashing in front of you'
+        'environment': 'Water rushing at splashing in front of you',
+        'north': 
+        'south':
+        'east':
+        'west':
     },
 
 ]
