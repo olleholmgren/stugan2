ca6b5aa18801cad95e8bd57faa3f2f8e53de235b
olleholmgren <olle.holmgren@gmail.com>
2023-03-01 08:43:04 +0000

    refresh rooms dictionary

--- rooms.py
+++ rooms.py
@@ -3,16 +3,19 @@ number = '0'
 
 rooms = [
     {
-        number: 1,
+        'number': 1,
         'name': 'The Tent',
+        'environment': 'green tent sheets(walls), a red sleeping bag, a hat with a feather'
     },
     {
-        number: 2,
+        'number': 2,
         'name': 'The Tent Entrance',
+        'environment': 'zip-lock'
     },
     {
-        number: 3,
+        'number': 3,
         'name': 'The river',
+        'environment': 'Water rushing at splashing in front of you'
     },
 
 ]
--- run.py
+++ run.py
@@ -3,11 +3,15 @@ from rooms import rooms
 
 
 
-print('\n--------------------------------\n--------------------------------\n\nWelcome to this peaceful and mysterious space in time\n\n--------------------------------\n--------------------------------\n')
+print('\n--------------------------------\n--------------------------------\n\nWelcome to this peaceful,\nmysterious space in time\n\n--------------------------------\n--------------------------------\n')
 
 player_name = str(input('Please enter your name:\n'))
 
 while player_name == type(str):
-    print(f'Nihaody {player_name}, how are you feeling today?')
-else: print('Please enter name')
+    try:
+        print(f'Nihaody {player_name}, how are you feeling today?')
+    except: print('Please enter name')
+
+
+
 
