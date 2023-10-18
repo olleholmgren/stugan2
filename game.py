9b0650e313e835ac155f2d1575e59f8dfb9747e2
olleholmgren <olle.holmgren@gmail.com>
2023-10-18 08:09:03 +0000

    typo in previous commit about adding function get_player_name. Add player name str input and if statement in get_player_name function to prevent from going further with blank name

--- game.py
+++ game.py
@@ -14,11 +14,13 @@ def intro():
     print_lines()
 
 def get_player_name():
-    player_name = str(input('Please enter your name'))
-
+    player_name = str(input('Please enter your name:\n'))
+    if player_name == '':
+        player_name = 'James Pond'
+        print('Well, as you did not tell me your name I will just call you James Pond')
+    print(f'Nihaody, {player_name}!')
 
 #condition = str(input(f'How are you feeling today {player_name}?\n'))
-
 #condition_follow_up1 = str(input(f'I see, why do you think you are feeling {condition}?\n'))
 #condition_follow_up2 = str(input('Alright. . . Well, do you want to talk to me about it?\n'))
 #print('Ok. I totally understand.')
@@ -31,11 +33,9 @@ def get_player_name():
 #print('You navigate by giving me a command of direction.\nn for north, s for' 
 #        'south, e for east and w for west.\nYou can answer questions with "yes" or "no"')
 
-
 def validate_answer(game_question):
     while True:
         print(game_question)
-
 
 def game():
     intro()
@@ -49,9 +49,6 @@ def game():
         return game_question
     if game_question == 'yes':
             return(rooms_list[1]['environment'])
-
-
-
 
 game()
 print(rooms_list[1]['environment'])
