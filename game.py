e8f67e3e44724b0efab6ab0881bbfe7790494210
olleholmgren <olle.holmgren@gmail.com>
2023-10-18 08:24:47 +0000

    add game beginning function with introduction questions after get_player_name within the game function

--- game.py
+++ game.py
@@ -1,6 +1,6 @@
 from rooms import rooms_list
 
-player_name = 'player'
+player_name = ''
 game_question = ''
 begin_game = ''
 
@@ -9,6 +9,7 @@ def print_lines():
     print('--------------------------------------------------')
 
 def intro():
+    
     print_lines()
     print('Welcome to this peaceful, mysterious space in time')
     print_lines()
@@ -17,10 +18,11 @@ def get_player_name():
     player_name = str(input('Please enter your name:\n'))
     if player_name == '':
         player_name = 'James Pond'
-        print('Well, as you did not tell me your name I will just call you James Pond')
+        print('Well, as you did not tell me your name I will just call you James Pond.')
     print(f'Nihaody, {player_name}!')
 
-#condition = str(input(f'How are you feeling today {player_name}?\n'))
+def beginning():
+    condition = str(input(f'How are you feeling today {player_name}?\n'))
 #condition_follow_up1 = str(input(f'I see, why do you think you are feeling {condition}?\n'))
 #condition_follow_up2 = str(input('Alright. . . Well, do you want to talk to me about it?\n'))
 #print('Ok. I totally understand.')
@@ -33,13 +35,14 @@ def get_player_name():
 #print('You navigate by giving me a command of direction.\nn for north, s for' 
 #        'south, e for east and w for west.\nYou can answer questions with "yes" or "no"')
 
-def validate_answer(game_question):
-    while True:
-        print(game_question)
+#def validate_answer(game_question):
+#    while True:
+#        print(game_question)
 
 def game():
     intro()
     get_player_name()
+    beginning()
     directions = ['n', 's', 'e', 'w']
     answer = ['yes', 'no']
 
