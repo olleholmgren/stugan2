7931ecdcf2a34e4bf60808c32cd13bcedd3c3e3e
olleholmgren <olle.holmgren@gmail.com>
2023-10-18 09:13:47 +0000

    uncomment code block from previous build and put it in the beginning function

--- game.py
+++ game.py
@@ -1,6 +1,6 @@
 from rooms import rooms_list
 
-player_name = ''
+player_name = 'player'
 game_question = ''
 begin_game = ''
 
@@ -9,7 +9,6 @@ def print_lines():
     print('--------------------------------------------------')
 
 def intro():
-    
     print_lines()
     print('Welcome to this peaceful, mysterious space in time')
     print_lines()
@@ -20,16 +19,17 @@ def get_player_name():
         player_name = 'James Pond'
         print('Well, as you did not tell me your name I will just call you James Pond.')
     print(f'Nihaody, {player_name}!')
+    return player_name
 
 def beginning():
-    condition = str(input(f'How are you feeling today {player_name}?\n'))
-#condition_follow_up1 = str(input(f'I see, why do you think you are feeling {condition}?\n'))
-#condition_follow_up2 = str(input('Alright. . . Well, do you want to talk to me about it?\n'))
-#print('Ok. I totally understand.')
-#game_question = str(input(f'Do you want to play an adventure game with me {player_name}?\n'))
-#if game_question == 'yes':
-#    print('ok, lets go!')
-#else: print('oh I see')
+    condition = str(input(f'How are you feeling today, {player_name}?\n'))
+    condition_follow_up1 = str(input(f'I see, why do you think you are feeling {condition}?\n'))
+    condition_follow_up2 = str(input('Alright. . . Well, do you want to talk to me about it?\n'))
+    print('Ok. I totally understand.')
+    game_question = str(input(f'Do you want to play an adventure game with me {player_name}?\n'))
+    if game_question == 'yes':
+        print('ok, lets go!')
+    else: print('oh I see')
 
 #print('Firstly, let me tell you how to play the game\n----------\n----------')
 #print('You navigate by giving me a command of direction.\nn for north, s for' 
